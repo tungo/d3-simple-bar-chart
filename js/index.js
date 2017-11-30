595b6468dc8e08be709127745b85f5e320a952c1
Tu Ngo <tungo8810@gmail.com>
2017-11-29 16:21:46 -0800

    Update svg chart

--- js/index.js
+++ js/index.js
@@ -1,19 +1,27 @@
 var data = [4, 8, 15, 16, 23, 42];
 
-var x = d3.scale.linear()
-  .domain([0, d3.max(data)])
-  .range([0, 420]);
+var width = 420,
+    barHeight = 20;
 
-document.addEventListener('DOMContentLoaded', function() {
-  d3.select('.chart')
-    .selectAll('div')
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, width]);
+
+var chart = d3.select(".chart")
+    .attr("width", width)
+    .attr("height", barHeight * data.length);
+
+var bar = chart.selectAll("g")
     .data(data)
-    .enter()
-    .append('div')
-    .style('width', function(d) {
-      return x(d) + 'px';
-    })
-    .text(function(d) {
-      return d;
-    });
-});
+  .enter().append("g")
+    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+bar.append("rect")
+    .attr("width", x)
+    .attr("height", barHeight - 1);
+
+bar.append("text")
+    .attr("x", function(d) { return x(d) - 3; })
+    .attr("y", barHeight / 2)
+    .attr("dy", ".35em")
+    .text(function(d) { return d; });
